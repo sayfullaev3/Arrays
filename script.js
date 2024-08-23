6c1c932a4573de5071b7e07c92e93a7a19bff6bf
sayfullaev3 <sayfullaevjasa22@gmail.com>
2024-08-23 16:24:03 +0500

    Update script.js

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-let list = ['johongir', "alan", "safina", "laylo", "azim", "farxod", "alisher", "jasur", "islom"]
+let list = ['johongir', "alan", "safina", "laylo", "azim", "alisher", "jasur", "islom"]
 let DeleteStudent = prompt('Введите имя студента которого хотите исключить: alan, azim, alisher, farhod, islom, jasur, jahongir, safina, laylo.').toLowerCase()
 let index = list.indexOf(DeleteStudent)
 if (list.includes(DeleteStudent)) {
